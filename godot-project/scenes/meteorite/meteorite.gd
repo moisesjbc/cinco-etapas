b62500c727a46cca48513c192dba886e93d9ef2b
Moisés J <moisesjbc@gmail.com>
2020-09-24 10:06:45 +0100

    Improved meteorite respawn + added removal based on distance to player.

--- godot-project/scenes/meteorite/meteorite.gd
+++ godot-project/scenes/meteorite/meteorite.gd
@@ -1,10 +1,21 @@
 extends RigidBody2D
 
+var player
+
 func _ready():
-	print(position)
+	player = get_node("/root/main_scene/player")
+
+	# Set a random linear velocity
 	var direction_vector: Vector2 = -position.normalized()
+	var intensity: float = 1.5
 	direction_vector.rotated(randf() * 0.3)
-	
-	var intensity: float = 3
 	add_force(Vector2(0.0, 0.0), direction_vector * intensity)
-	angular_velocity = 2
+	
+	# Set a random linear velocity
+	angular_velocity = randf() - 0.5
+	
+
+func _physics_process(delta):
+	# Delete meteorite if too far from player
+	if global_position.distance_to(player.global_position) > 1000:
+		queue_free()
