43c25ea1e901cf3aa364936ffe919d239530afb6
Moisés J <moisesjbc@gmail.com>
2020-09-27 17:26:22 +0100

    Minor fixes + fixed links in credits menu

--- godot-project/menus/credits_menu/credits_menu.gd
+++ godot-project/menus/credits_menu/credits_menu.gd
@@ -3,3 +3,7 @@ extends Control
 
 func _on_go_back_button_pressed():
 	get_tree().change_scene("res://menus/main_menu/main_menu.tscn")
+
+
+func _on_credits_body_meta_clicked(meta):
+	OS.shell_open(meta)
