b5eac97c45383e88ddd20fc2ffef763b384cf2f5
Moisés J <moisesjbc@gmail.com>
2020-09-27 14:03:30 +0100

    Minor fixes + updated ending texts

--- godot-project/scenes/ending/ending.gd
+++ godot-project/scenes/ending/ending.gd
@@ -1,11 +1,16 @@
 extends Node2D
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$story_gui.set_texts([
-		"Hace unos meses desde el accidente",
-		"Lo pagué con mis amigos",
-		"Pero fueron ellos quienes me ayudadon a aceptarlo",
-		"y me sacaron del pozo",
-		"Gracias"
-	])
+		"Hace unos meses desde el accidente.",
+		"Al principio me negué a aceptarlo.",
+		"Y lo pagué con mis amigos.",
+		"Lo intenté todo para recuperarlo.",
+		"Pero fue imposible y me rendí.",
+		"Mis amigos no se rindieron conmigo y me ayudaron a aceptarlo",
+		"\"Lo... siento\", me disculpé",
+		"\"Tranquilo, solo fue una etapa\"",
+	], funcref(self, "return_to_main_menu"))
+	
+func return_to_main_menu():
+	get_tree().change_scene("res://menus/main_menu/main_menu.tscn")
