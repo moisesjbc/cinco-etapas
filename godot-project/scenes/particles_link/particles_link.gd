8bfdefa71dc39db80864a5abcdfab6a29964cab3
Moisés J <moisesjbc@gmail.com>
2020-09-27 13:49:09 +0100

    Removed debug message

--- godot-project/scenes/particles_link/particles_link.gd
+++ godot-project/scenes/particles_link/particles_link.gd
@@ -6,7 +6,6 @@ func _ready():
 func link(src_node, dst_node):
 	position = src_node.position
 	gravity = to_local(dst_node.global_position) - src_node.position
-	print("src_node.get_parent().name ", src_node.get_parent().name)
 	if src_node.get_parent().name != "loved_one":
 		lifetime = gravity.length() * 0.015
 	scale = Vector2.ONE
