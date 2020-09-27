b93dc09efd64e48b80c56ffac058d6c8945bf869
Moisés J <moisesjbc@gmail.com>
2020-09-27 16:35:50 +0100

    Added exit button

--- godot-project/menus/main_menu/main_menu.gd
+++ godot-project/menus/main_menu/main_menu.gd
@@ -1,8 +1,16 @@
 extends Control
 
+func _ready():
+	if OS.get_name() == "HTML5":
+		$margin_container/root_vbox_container/vbox_container/exit_button.visible = false
+
 func _on_play_button_pressed():
 	get_tree().change_scene("res://scenes/main_scene/main_scene.tscn")
 
 
 func _on_credits_button_pressed():
 	get_tree().change_scene("res://menus/credits_menu/credits_menu.tscn")
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()
