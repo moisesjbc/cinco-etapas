c1c4b953af1a3daccd9887dd3682f0bc0ba2886c
Moisés J <moisesjbc@gmail.com>
2020-09-27 13:19:13 +0100

    Added main menu button to game over

--- godot-project/menus/game_over/game_over.gd
+++ godot-project/menus/game_over/game_over.gd
@@ -3,3 +3,7 @@ extends MarginContainer
 func _on_restart_button_pressed():
 	set_visible(false)
 	get_tree().change_scene("res://scenes/main_scene/main_scene.tscn")
+
+
+func _on_go_to_main_menu_button_pressed():
+	get_tree().change_scene("res://menus/main_menu/main_menu.tscn")
